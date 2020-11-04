9991e9d7f2c9a1d19103d15f733424ef8ca2d3fd
Salman Khan <salman0401khan@gmail.com>
2020-11-04 23:36:51 +0530

    Port Update

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var app = require('./app');
-var port = process.env.NODE_PORT || 5000;
+var port = process.env.PORT || 5000;
 app.listen(port, (err) => {
 	if (err) console.error('Error while starting the app:', err);
 	else {
